b1942543f05af42ce2feaa6a69f14510beda494a
slbtty(openSUSE) <shenlebantongying@gmail.com>
2021-03-22 00:47:26 -0400

    Score board

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include <iostream>
+
+/* TODO: fix CMAKE pkg-config
+ */
+
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
@@ -18,18 +23,16 @@ using BALL = struct {
     int dir_y;
 };
 
-using player = struct {
-    SDL_Rect pos;
-    int score;
-    int speed;
-};
 
 
 int main(int argc, char **argv) {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) sdl_error("failed to init");
 
+    SDL_Color White = {255, 255, 255};
 
     SDL_ShowCursor(SDL_DISABLE);
+
+
 
     SDL_Window *win = SDL_CreateWindow(
             "Pong game",
@@ -53,7 +56,9 @@ int main(int argc, char **argv) {
     const Uint8 *keyState = SDL_GetKeyboardState(nullptr);// Global Keyboard handler
     int mouse_x, mouse_y;
     Uint32 last_update_time = 0;
-    auto ball = BALL{SDL_Rect{0, 0, 10, 10}, 2, 2, 1, 1};
+    auto ball = BALL{SDL_Rect{10, 10, 10, 10},
+                     4, 4,
+                     1, 1};
 
     const int bw = 3;
 
@@ -61,6 +66,15 @@ int main(int argc, char **argv) {
     auto window_top_border = SDL_Rect{0, 0, SCREEN_WIDTH, bw};
     auto window_left_border = SDL_Rect{0, 0, bw, SCREEN_HEIGHT};
     auto window_right_border = SDL_Rect{SCREEN_WIDTH - bw, 0, bw, SCREEN_HEIGHT};
+
+
+    TTF_Init();
+    TTF_Font *sans_font = TTF_OpenFont("OpenSans-Regular.ttf", 50);
+    SDL_Surface *textSurface;
+    SDL_Texture *textTexture;
+    SDL_Rect text_rect;
+
+    int score{0};
 
     bool quit{};
     SDL_Event event;
@@ -85,7 +99,7 @@ int main(int argc, char **argv) {
         SDL_RenderFillRect(ren, &window_right_border);
 
 
-        if (SDL_TICKS_PASSED(SDL_GetTicks(), last_update_time + 10)) {
+        if (SDL_TICKS_PASSED(SDL_GetTicks(), last_update_time + 5)) {
             ball.rect.x += ball.vx * ball.dir_x;
             ball.rect.y += ball.vy * ball.dir_y;
             last_update_time = SDL_GetTicks();
@@ -94,15 +108,27 @@ int main(int argc, char **argv) {
 
 
         if (SDL_HasIntersection(&ball.rect, &window_bottom_border)) {
-            std::cout << "game over" << std::endl;
+            std::cout << "game over:" << score << std::endl;
+            break;
         }
 
-        if (SDL_HasIntersection(&ball.rect, &window_top_border) || SDL_HasIntersection(&ball.rect, &paddle)) {
+        if (SDL_HasIntersection(&ball.rect, &window_top_border)) {
             ball.dir_y *= -1;
         }
+
+        if (SDL_HasIntersection(&ball.rect, &paddle)){
+            ball.dir_y *= -1;
+            score++;;
+        }
+
         if (SDL_HasIntersection(&ball.rect, &window_left_border) || SDL_HasIntersection(&ball.rect, &window_right_border)) {
             ball.dir_x *= -1;
         }
+
+        textSurface = TTF_RenderText_Blended(sans_font, ("Score: "+ std::to_string(score)).c_str(), White);
+        textTexture = SDL_CreateTextureFromSurface(ren, textSurface);
+        SDL_RenderCopy(ren, textTexture, nullptr, &text_rect);
+        text_rect = SDL_Rect {50,50,textSurface->w,textSurface->h};
 
         //Show all the has been done behind the scenes
         SDL_RenderPresent(ren);
